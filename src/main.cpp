07f0bbf570bbd2964097b1bbee4a141a8141ae28
serdaraltin <seyupaltin@gmail.com>
2024-09-10 01:12:07 +0300

    more more

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,27 @@
 #include "image-process/FrameProcessor.hpp"
 #include "gui/DisplayManager.hpp"
 
+using namespace cv;
+using namespace std;
+using namespace libfreenect2;
+
+
 void processFrames(FrameProcessor& frame_processor, KinectDevice& kinect) {
-    libfreenect2::FrameMap frames;
+    FrameMap frames;
     if (!kinect.getListener().waitForNewFrame(frames, 10 * 1000)) {
-        std::cout << "Timeout!" << std::endl;
+        cout << "Timeout!" << endl;
         return;
     }
 
-    libfreenect2::Frame* rgb = frames[libfreenect2::Frame::Color];
-    libfreenect2::Frame* depth = frames[libfreenect2::Frame::Depth];
+    Frame* rgb = frames[Frame::Color];
+    Frame* depth = frames[Frame::Depth];
 
-    libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4);
+    Frame undistorted(512, 424, 4), registered(512, 424, 4);
     frame_processor.applyRegistration(rgb, depth, &undistorted, &registered);
 
-    cv::Mat rgb_frame = cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data);
-    cv::Mat depth_frame = frame_processor.processDepthFrame(depth);
-    cv::Mat ir_frame = frame_processor.processIrFrame(&registered);
+    Mat rgb_frame = Mat(rgb->height, rgb->width, CV_8UC4, rgb->data);
+    Mat depth_frame = frame_processor.processDepthFrame(depth);
+    Mat ir_frame = frame_processor.processIrFrame(&registered);
 
     DisplayManager::showFrames(rgb_frame, depth_frame, ir_frame);
 
@@ -28,11 +33,11 @@ void processFrames(FrameProcessor& frame_processor, KinectDevice& kinect) {
 int main() {
     FileLogger file_logger("LOGFILE");
     if (!file_logger.good()) {
-        std::cerr << "Failed to open log file." << std::endl;
+        cerr << "Failed to open log file." << endl;
         return -1;
     }
     setGlobalLogger(&file_logger);
-    std::cout << "Log file created." << std::endl;
+    cout << "Log file created." << endl;
 
     KinectDevice kinect;
     if (!kinect.initialize()) return -1;
@@ -42,7 +47,7 @@ int main() {
     while (true) {
         processFrames(frame_processor, kinect);
 
-        int key = cv::waitKey(1);
+        int key = waitKey(1);
         if (key == 'q') break;
     }
 
