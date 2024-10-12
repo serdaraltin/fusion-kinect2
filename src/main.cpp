b11fbb4bc39df7ca2251a36e5437c7641ae8fd2e
serdaraltin <seyupaltin@gmail.com>
2024-10-13 00:47:41 +0300

    fixed cmake version and Boost implementation edit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include "device/device_manager.h"
 #include "logger/console_logger.h"
-#include <boost/foreach.hpp>
-#include <iostream>
 
 using namespace vision;
 
@@ -10,12 +8,6 @@ ConsoleLogger* console_logger = ConsoleLogger::getInstance();
 
 int main(int argc, char *argv[])
 {
-    std::string hello("Hello, world!");
-
-    BOOST_FOREACH( char ch, hello){
-        std:: cout << ch;
-    }
-
     console_logger->log(Logger::Info, "Vision Finised.");
     return 0;
 }
