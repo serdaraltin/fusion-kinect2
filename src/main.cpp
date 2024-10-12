d2b8aef0cedc030d6e613a9546eeb0119d381668
serdaraltin <seyupaltin@gmail.com>
2024-10-13 00:05:44 +0300

    add boost libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "device/device_manager.h"
 #include "logger/console_logger.h"
+#include <boost/foreach.hpp>
+#include <iostream>
 
 using namespace vision;
 
@@ -8,6 +10,11 @@ ConsoleLogger* console_logger = ConsoleLogger::getInstance();
 
 int main(int argc, char *argv[])
 {
+    std::string hello("Hello, world!");
+
+    BOOST_FOREACH( char ch, hello){
+        std:: cout << ch;
+    }
 
     console_logger->log(Logger::Info, "Vision Finised.");
     return 0;
